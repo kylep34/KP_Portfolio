bb55a581937db49995dcdaf7668099f3e867a351
kylep34 <74076509+kylep34@users.noreply.github.com>
2021-11-10 21:32:23 -0500

    Portfolio
    Updated portfolio with added cariousel feature

--- js/index.js
+++ js/index.js
@@ -13,6 +13,24 @@ function scrollSkills(e)
 }
 
 
+var modal = document.getElementById("cs3Screen1Modal");
+
+var img = document.getElementsByClassName("cs3Screen11");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function()
+{
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    captionText.innerHTML = this.alt; 
+}
+
+var span = document.getElementsByClassName("close")[0];
+
+span.onclick = function() { 
+    modal.style.display = "none";
+}
+
 
 
 
